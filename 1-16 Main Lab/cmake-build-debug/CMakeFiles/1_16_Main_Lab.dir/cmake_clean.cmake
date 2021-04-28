9ea1dce32727b42e0b9ee544ec3ceb4d18150189
sdaffer <jacob.feltman@gmail.com>
2021-04-28 13:06:48 -0600

    inital setup commit

---
+++ 1-16 Main Lab/cmake-build-debug/CMakeFiles/1_16_Main_Lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1_16_Main_Lab"
+  "1_16_Main_Lab.pdb"
+  "CMakeFiles/1_16_Main_Lab.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_16_Main_Lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1-16 Main Lab/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Display the information on a neatly formatted table, with proper \t escape sequence.
+    cout << "CS 142 Main Lab Options" << endl;
+    cout << "Lab Number" << "\t" << "Variation" << "\t" << "Title" << endl;
+    cout << "1" << "\t\t" << "A" << "\t\t" << "Do as I'm Doing" << endl;
+    cout << "2" << "\t\t" << "A" << "\t\t" << "Pizza" << endl;
+    cout << "3" << "\t\t" << "A" << "\t\t" << "Fun with Functions" << endl;
+    cout << "4" << "\t\t" << "A" << "\t\t" << "Multipath Adventure Game" << endl;
+    cout << "5" << "\t\t" << "A" << "\t\t" << "Payment Calculator" << endl;
+    cout << "6" << "\t\t" << "A" << "\t\t" << "Dot Product Dating" << endl;
+    cout << "7" << "\t\t" << "A" << "\t\t" << "Online Shopping Cart" << endl;
+    cout << "8" << "\t\t" << "A" << "\t\t" << "Playlists";
+    return 0;
+}
