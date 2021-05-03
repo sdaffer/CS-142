d353720b364a0ee3b7e1fa469440c87d289bcb53
sdaffer <jacob.feltman@gmail.com>
2021-05-03 17:31:34 -0600

    Solution.

--- 5-8-prep-lab/5-8-prep-lab.cpp
+++ 5-8-prep-lab/5-8-prep-lab.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char triangleChar = ' ';
+    int triangleHeight = 0;
+
+    cout << "Enter a character: " << endl;
+    cin >> triangleChar;
+
+    cout << "Enter triangle height: " << endl;
+    cin >> triangleHeight;
+
+    string charString = "";
+    for (int i = 0; i < triangleHeight; ++i) {
+        charString.push_back(triangleChar);
+        charString.push_back(' ');
+        cout << charString << endl;
+    }
     return 0;
 }
