e0222331c76fcd7115d2e76c9f13f00192b15878
sdaffer <jacob.feltman@gmail.com>
2021-05-03 12:25:48 -0600

    Update - PromptHandler() - 0.1

--- 4-26-main-lab/4-26-main-lab.cpp
+++ 4-26-main-lab/4-26-main-lab.cpp
@@ -14,6 +14,7 @@ int main() {
     cout << "CAUTION: I am spectacularly average at chess. Anyone who really knows about the game and plays this adventure will probably laugh at my incompetence." << endl;
     cout << "That's okay, just a warning." << endl;
 
+    // FIXME: This is a test call.
     cout << PromptHandler("This is the prompt: ", 0);
 
     return 0;
@@ -29,13 +30,35 @@ int PromptHandler(string strPrompt, int optionsAvailable){
         cin.clear();
         cin.ignore(1000, '\n');
         cout << "Invalid input. Try again." << endl;
-        // cout << strPrompt;
-        // cin >> userInput;
+        // Call the PromptHandler again to loop.
         PromptHandler(strPrompt, optionsAvailable);
     }
 
     // Making sure only the number of available options will be used
-
-
+    switch(optionsAvailable){
+        case 0:
+            cout << "There should be at least one option available" << endl;
+            break;
+        case 1:
+            if (userInput > 1){
+                cout << "Invalid input. Try again." << endl;
+            }
+            break;
+        case 2:
+            if (userInput > 2){
+                cout << "Invalid input. Try again." << endl;
+            }
+            break;
+        case 3:
+            if (userInput > 3){
+                cout << "Invalid input. Try again." << endl;
+            }
+            break;
+        default:
+            break;
+    }
+    if (userInput >= 4){
+        cout << "Invalid input. There are never more than 3 options in this program. Please try again." << endl;
+    }
     return userInput;
 }
