985b135c53d2be783cd0cd49b5b7c931c5a9e919
sdaffer <jacob.feltman@gmail.com>
2021-05-04 12:59:08 -0600

    Complete solution.

--- 5-17-prep-lab/5-17-prep-lab.cpp
+++ 5-17-prep-lab/5-17-prep-lab.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
     int arrowBaseHeight = 0;
     int arrowBaseWidth = 0;
     int arrowHeadWidth = 0;
+    int currentArrowHeadWidth = 0;
 
-    cout << "Enter arrow base height: " << endl;
-    cin >> arrowBaseHeight;
+        cout << "Enter arrow base height: " << endl;
+        cin >> arrowBaseHeight;
 
-    cout << "Enter arrow base width: " << endl;
-    cin >> arrowBaseWidth;
+        cout << "Enter arrow base width: " << endl;
+        cin >> arrowBaseWidth;
 
-    cout << "Enter arrow head width: " << endl;
-    cin >> arrowHeadWidth;
+        cout << "Enter arrow head width: " << endl;
+        cin >> arrowHeadWidth;
 
-    // Draw arrow base (height = 3, width = 2)
-    cout << "**" << endl;
-    cout << "**" << endl;
-    cout << "**" << endl;
+    while (arrowHeadWidth <= arrowBaseWidth) {
+        cout << "Enter arrow head width: " << endl;
+        cin >> arrowHeadWidth;
+    }
 
-    // Draw arrow head (width = 4)
-    cout << "****" << endl;
-    cout << "***" << endl;
-    cout << "**" << endl;
-    cout << "*" << endl;
+    for (int i = 0; i < arrowBaseHeight; ++i) {
+        for (int j = 0; j < arrowBaseWidth; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    currentArrowHeadWidth = arrowHeadWidth;
+    for (int i = 0; i < arrowHeadWidth; ++i) {
 
+        for (int j = 0; j < currentArrowHeadWidth; ++j) {
+           cout << "*";
+        }
+        currentArrowHeadWidth = currentArrowHeadWidth - 1;
+        cout << endl;
+    }
     return 0;
 }
