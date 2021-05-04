4eabe8b502b2b102c9ec6054946ba8f7724c903e
sdaffer <jacob.feltman@gmail.com>
2021-05-04 16:42:53 -0600

    Final solution

--- 5-18-main-lab/5-18-main-lab.cpp
+++ 5-18-main-lab/5-18-main-lab.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 // Function prototypes so main() is easier to read
-// FIXME: Think about changing the appropriate parameters to constants
-double PaymentSimulation(double initialLoan, double lengthPayback, double interestRate, double initialGuess); //FIXME: Does initialGuess need to be a constant or variable?
-double ExactCalculation(const double initialLoan, const int lengthPayback, const double interestRate);
+double PaymentSimulation(double initialLoan, double lengthPayback, double interestRate, double initialGuess);
+double ExactCalculation(double initialLoan, int lengthPayback, double interestRate);
 
 // Global variables
+const double MONTHS_IN_YEAR = 12.0;
 
 int main() {
     // Variables
@@ -17,6 +17,7 @@ int main() {
     double interestUserRate = 0.0;
     double initialUserGuess = 0.0;
     double returnedAmountOwed = 0.0;
+
 
     // Prompt user for inputs
     cout << "Enter initial loan amount: ";
@@ -29,59 +30,63 @@ int main() {
 
     cout << "Enter length of payback period (in years): ";
     cin >> lengthUserPayback;
-    cout << lengthUserPayback << endl;
+    cout << endl;
     cout << endl;
 
     cout << "Guess monthly payment: ";
     cin >> initialUserGuess;
     cout << endl << endl;
 
-    //FIXME: Use one for and one while loop
 
     // Payment simulation loop
     returnedAmountOwed = PaymentSimulation(initialUserLoanAmount, lengthUserPayback, interestUserRate, initialUserGuess);
-    while (returnedAmountOwed != 0) {
-        cout << "It's working!" << endl;
-    }
+    do {
+        // Instructions on how to update the initial guess if there's still a balance (paid too little)
+        if (returnedAmountOwed > 0) {
+            initialUserGuess = initialUserGuess + (returnedAmountOwed / (MONTHS_IN_YEAR * lengthUserPayback));
+            returnedAmountOwed = PaymentSimulation(initialUserLoanAmount, lengthUserPayback, interestUserRate, initialUserGuess);
+        }
+        // Instructions on how to update the initial guess if paid too much
+        else if (returnedAmountOwed < 0) {
+            initialUserGuess = initialUserGuess + (returnedAmountOwed / (MONTHS_IN_YEAR * lengthUserPayback));
+            returnedAmountOwed = PaymentSimulation(initialUserLoanAmount, lengthUserPayback, interestUserRate, initialUserGuess);
+        }
+    } while (fabs(returnedAmountOwed) >= 0.01);
+
 
     // Output result of simulation. Precision is set to round to nearest cent.
     cout << fixed << setprecision(2);
-    cout << "Final monthly payment: $" << "" << endl; //FIXME: Fill in blank with the output from algorithm
-    cout << "Final amount owed: $" << returnedAmountOwed << endl; //FIXME: Fill in blank with the output from algorithm
+    cout << "Final monthly payment: $" << initialUserGuess << endl;
+    cout << "Final amount owed: $" << returnedAmountOwed << endl;
+
 
     // Verification with exact calculation. Precision is set to round to nearest cent.
     cout << "Monthly payment (from formula): $";
     cout << fixed << setprecision(2) << ExactCalculation(initialUserLoanAmount, lengthUserPayback, interestUserRate);
-    
+
+
     return 0;
 }
 
 
 // Payment simulation function
-// FIXME: Is PaymentSimulation() supposed to be a double?
-// FIXME: Make sure that int lengthPayback doesn't need to be double lengthPayback
-// FIXME: Think about changing the appropriate parameters to constants
 double PaymentSimulation(double initialLoan, double lengthPayback, double interestRate, double initialGuess) {
     // Using a double here to prevent possible integer division somewhere
     double numPayments = 0.0;
     double debtLeft = initialLoan;
 
-    numPayments = lengthPayback * 12;
+    numPayments = lengthPayback * MONTHS_IN_YEAR;
 
     for (int i = 0; i < numPayments; ++i) {
-        debtLeft = ((1 + (interestRate / 12.0)) * debtLeft) - initialGuess;
-        cout << debtLeft << endl; //FIXME: remove this when done testing
+        debtLeft = ((1 + (interestRate / MONTHS_IN_YEAR)) * debtLeft) - initialGuess;
     }
-
-
 
     return debtLeft;
 }
+
 // Exact calculation function
-// FIXME: Make sure that int lengthPayback doesn't need to be double lengthPayback
-// FIXME: Think about changing the appropriate parameters to constants
 double ExactCalculation(double initialLoan, const int lengthPayback, const double interestRate) {
     double monthlyPayment;
-    monthlyPayment = ((1 - (1 + (interestRate / 12))) / ((pow(1 + (interestRate / 12), -12 * lengthPayback)) - 1)) * initialLoan;
+    monthlyPayment = ((1 - (1 + (interestRate / MONTHS_IN_YEAR))) / ((pow(1 + (interestRate / MONTHS_IN_YEAR), -MONTHS_IN_YEAR * lengthPayback)) - 1)) * initialLoan;
     return monthlyPayment;
 }
