6198e204a6a2e927f1a44f04b83d1cd56c2fd1df
sdaffer <jacob.feltman@gmail.com>
2021-05-05 12:31:59 -0600

    Rough outline of plan/solution

--- 6-11-prep-lab/6-11-prep-lab.cpp
+++ 6-11-prep-lab/6-11-prep-lab.cpp
@@ -2,9 +2,49 @@
 #include <vector>
 using namespace std;
 
+
+// Function prototypes
+void outputVector();
+double totalWeight(vector<double> inputVector);
+double averageWeight(vector<double> inputVector);
+double maxWeight(vector<double> inputVector);
+
+
+// main()
 int main() {
 
-    /* Type your code here. */
+    // Variables
+    const int NUM_WEIGHTS = 5;
+    vector<double> peopleWeights(NUM_WEIGHTS);
+
+    // Prompt user for input of the five weights
+    for (int i = 0; i < peopleWeights.size(); ++i) {
+        cout << "Enter weight " << i + 1 << ": ";
+        cin >> peopleWeights.at(i);
+    }
+
 
     return 0;
+}
+
+
+// Functions
+void outputVector() {
+
+}
+
+double totalWeight(vector<double> inputVector) {
+    double returnedTotalWeight = 0.0;
+
+
+
+    return returnedTotalWeight;
+}
+
+double averageWeight(vector<double> inputVector) {
+
+}
+
+double maxWeight(vector<double> inputVector) {
+
 }
