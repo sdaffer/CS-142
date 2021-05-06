dd80a4e9433a80e4b953764c9a23e6dd4deae58c
sdaffer <jacob.feltman@gmail.com>
2021-05-06 15:47:26 -0600

    Update

--- 6-29-prep-lab/6-29-prep-lab.cpp
+++ 6-29-prep-lab/6-29-prep-lab.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 
 // Function Prototypes
+char userPromptAndInput();
+void outputPlayersAbove(const vector<int>& playerNumber, const vector<int>& playerRating);
+void outputRoster(const vector<int>& playerNumber, const vector<int>& playerRating);
+void deletePlayer(vector<int>& playerNumber, vector<int>& playerRating);
+void updateRating(vector<int>& playerNumber, vector<int>& playerRating);
+void addPlayer(vector<int>& playerNumber, vector<int>& playerRating);
 
-
-
+// Main function
 int main() {
 
     // Variables
@@ -31,6 +36,85 @@ int main() {
     }
 
     cout << endl;
+
+
+
+    return 0;
+}
+
+
+// Functions
+
+void addPlayer(vector<int>& playerNumber, vector<int>& playerRating) {
+    int userInputNumber = 0;
+    int userInputRating = 0;
+
+    cout << "Enter another player's jersey number: ";
+    cin >> userInputNumber;
+    cout << "Enter another player's rating: ";
+    cin >> userInputRating;
+
+    playerNumber.push_back(userInputNumber);
+    playerRating.push_back(userInputRating);
+}
+
+void updateRating(vector<int>& playerNumber, vector<int>& playerRating) {
+    int userJerseyInput = 0;
+    int userRatingInput = 0;
+
+    cout << "Enter a jersey number: ";
+    cin >> userJerseyInput;
+    cout << "Enter a new rating for player: ";
+    cin >> userRatingInput;
+
+    for (int i = 0; i < playerRating.size(); ++i) {
+        if (playerNumber.at(i) == userJerseyInput) {
+            playerRating.at(i) = userRatingInput;
+        }
+    }
+}
+
+void deletePlayer(vector<int>& playerNumber, vector<int>& playerRating) {
+    int userJerseyInput = 0;
+
+    cout << "Enter a jersey number: ";
+    cin >> userJerseyInput;
+
+    for (int i = 0; i < playerNumber.size(); ++i) {
+        if (playerNumber.at(i) == userJerseyInput) {
+            playerNumber.erase(playerNumber.begin() + i);
+            playerRating.erase(playerRating.begin() + i);
+        }
+    }
+}
+
+void outputRoster(const vector<int>& playerNumber, const vector<int>& playerRating) {
+    cout << "ROSTER" << endl;
+
+    for (int i = 0; i < playerNumber.size(); ++i) {
+        cout << "Player " << i + 1 << " -- Jersey number: " << playerNumber.at(i) << ", ";
+        cout << "Rating: " << playerRating.at(i) << endl;
+    }
+}
+
+void outputPlayersAbove(const vector<int>& playerNumber, const vector<int>& playerRating) {
+    int userInput = 0;
+
+    cout << "Enter a rating: ";
+    cin >> userInput;
+    cout << "ABOVE " << userInput << endl;
+
+    for (int i = 0; i < playerRating.size(); ++i) {
+        if (playerRating.at(i) > userInput) {
+            cout << "Player " << i + 1 << " -- Jersey number: " << playerNumber.at(i) << ", ";
+            cout << "Rating: " << playerRating.at(i) << endl;
+        }
+    }
+}
+
+char userPromptAndInput(){
+    char inputChar = 'a';
+
     cout << "MENU" << endl;
     cout << "a - Add player" << endl;
     cout << "d - Remove player" << endl;
@@ -40,28 +124,8 @@ int main() {
     cout << "q - Quit" << endl;
     cout << endl;
 
-
-    return 0;
-}
-
-
-// Functions
-
-void addPlayer(const char inputChar, vector<int>& playerNumber, vector<int>& playerRating) {
-    int userInputNumber = 0;
-    int userInputRating = 0;
-
-    cout << "Enter another player's jersey number: ";
-    cin >> userInputNumber;
-    cout << "Enter another player's rating: ";
-    cin >> userInputRating;
-
-
-}
-
-char userInput(){
-    char inputChar = 'a';
     cout << "Choose an option: ";
     cin >> inputChar;
+
     return inputChar;
 }
