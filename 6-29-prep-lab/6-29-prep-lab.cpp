0e8dcb24a4b14f70721dc3f71f2fecb782a9669b
sdaffer <jacob.feltman@gmail.com>
2021-05-06 15:26:21 -0600

    Update

--- 6-29-prep-lab/6-29-prep-lab.cpp
+++ 6-29-prep-lab/6-29-prep-lab.cpp
@@ -1,6 +1,67 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+
+// Function Prototypes
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // Variables
+    const int NUM_PAIRS = 5;
+    vector<int> playerNumber(NUM_PAIRS);
+    vector<int> playerRating(NUM_PAIRS);
+
+    for (int i = 0; i < NUM_PAIRS; ++i) {
+        cout << "Enter player " << i + 1 << "'s jersey number: ";
+        cin >> playerNumber.at(i);
+        cout << "Enter player " << i + 1 << "'s rating: ";
+        cin >> playerRating.at(i);
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "ROSTER" << endl;
+
+    for (int i = 0; i < NUM_PAIRS; ++i) {
+        cout << "Player " << i + 1 << " -- Jersey number: " << playerNumber.at(i) << ", ";
+        cout << "Rating: " << playerRating.at(i) << endl;
+    }
+
+    cout << endl;
+    cout << "MENU" << endl;
+    cout << "a - Add player" << endl;
+    cout << "d - Remove player" << endl;
+    cout << "u - Update player rating" << endl;
+    cout << "r - Output players above a rating" << endl;
+    cout << "o - Output roster" << endl;
+    cout << "q - Quit" << endl;
+    cout << endl;
+
+
     return 0;
+}
+
+
+// Functions
+
+void addPlayer(const char inputChar, vector<int>& playerNumber, vector<int>& playerRating) {
+    int userInputNumber = 0;
+    int userInputRating = 0;
+
+    cout << "Enter another player's jersey number: ";
+    cin >> userInputNumber;
+    cout << "Enter another player's rating: ";
+    cin >> userInputRating;
+
+
+}
+
+char userInput(){
+    char inputChar = 'a';
+    cout << "Choose an option: ";
+    cin >> inputChar;
+    return inputChar;
 }
