3736d96d13103f4ba7c9bea9d50dce3d3fc56036
sdaffer <jacob.feltman@gmail.com>
2021-05-07 15:21:35 -0600

    Working on the problem

--- 7-18-2-challenge-activity/7-18-2-challenge-activity.cpp
+++ 7-18-2-challenge-activity/7-18-2-challenge-activity.cpp
@@ -1,6 +1,77 @@
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Song {
+public:
+    void SetDurationAndName(int songDuration, string songName) {
+        duration = songDuration;
+        name = songName;
+    }
+    void PrintSong() const {
+        cout << duration << " - " << name << endl;
+    }
+    int GetDuration() const { return duration; }
+    string GetName() const { return name; }
+
+private:
+    int duration;
+    string name;
+};
+
+class Album {
+public:
+    void SetName(string albumName) { name = albumName; }
+    void InputSongs();
+    void PrintName() const { cout << name << endl; }
+    void PrintSongsShorterThan(int songDuration) const;
+
+private:
+    string name;
+    vector<Song> albumSongs;
+};
+
+void Album::InputSongs() {
+    Song currSong;
+    int currDuration;
+    string currName;
+
+    cin >> currDuration;
+    while (currDuration >=  0) {
+        getline(cin, currName);
+        currSong.SetDurationAndName(currDuration, currName);
+        albumSongs.push_back(currSong);
+        cin >> currDuration;
+    }
+}
+
+void Album::PrintSongsShorterThan(int songDuration) const {
+    unsigned int i;
+    Song currSong;
+
+    cout << "Songs shorter than " << songDuration << " seconds:" << endl;
+
+    /* Your code goes here */
+    for (i = 0; i < albumSongs.size(); ++i) {
+        //while (albumSongs.currSong.GetDuration() < songDuration) {
+        //while (currSong.GetDuration() < songDuration) {
+        while (albumSongs.GetDuration() < songDuration) {
+            currSong.PrintSong();
+        }
+    }
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Album musicAlbum;
+    string albumName;
+
+    getline(cin, albumName);
+    musicAlbum.SetName(albumName);
+    musicAlbum.InputSongs();
+    musicAlbum.PrintName();
+    musicAlbum.PrintSongsShorterThan(150);
+
     return 0;
 }
