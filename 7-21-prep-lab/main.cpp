537b43ae2e6f2bd00e344b41dd37ed1f7d4d8e33
sdaffer <jacob.feltman@gmail.com>
2021-05-07 16:22:43 -0600

    Total sum update

--- 7-21-prep-lab/main.cpp
+++ 7-21-prep-lab/main.cpp
@@ -7,6 +7,7 @@ int main() {
     ItemToPurchase secondItem;
     string currItemName = "";
     double currItemPrice = 0.0;
+    double priceTotal = 0.0;
     int currItemQuantity = 0;
 
     /*Prompt user for two items and put them into both objects */
@@ -31,10 +32,28 @@ int main() {
     cin >> currItemPrice;
     cout << "Enter the item quantity: ";
     cin >> currItemQuantity;
+    cout << endl;
 
     secondItem.SetName(currItemName);
     secondItem.SetPrice(currItemPrice);
     secondItem.SetQuantity(currItemQuantity);
 
+    /*Sum up total cost for both items and output*/
+    // Item1
+    cout << "TOTAL COST" << endl;
+    cout << firstItem.GetName() << " " << firstItem.GetQuantity() << " @ $";
+    cout << firstItem.GetPrice() << " = $";
+    priceTotal = priceTotal + firstItem.GetPrice() * firstItem.GetQuantity();
+    cout << priceTotal << endl;
 
+    // Item2
+    cout << secondItem.GetName() << " " << secondItem.GetQuantity() << " @ $";
+    cout << secondItem.GetPrice() << " = $";
+    priceTotal = priceTotal + secondItem.GetPrice() * secondItem.GetQuantity();
+    cout << priceTotal << endl;
+
+    cout << endl;
+    cout << "Total: $" << priceTotal;
+
+    return 0;
 }
