e3ad82ed4633636080f6490ea982f7e2ed130dfe
sdaffer <jacob.feltman@gmail.com>
2021-05-08 13:32:49 -0600

    Added member functions to ItemToPurchase class

--- 7-22-main-lab/ItemToPurchase.cpp
+++ 7-22-main-lab/ItemToPurchase.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 #include "ItemToPurchase.h"
 
-
+// Setters
 void ItemToPurchase::SetName(string nameParam) {
     itemName = nameParam;
 }
@@ -13,15 +14,40 @@ void ItemToPurchase::SetPrice(double priceParam) {
 void ItemToPurchase::SetQuantity(int quantityParam) {
     itemQuantity = quantityParam;
 }
+void ItemToPurchase::SetDescription(string descriptionParam) {
+    itemDescription = descriptionParam;
+}
 
+// Getters
 string ItemToPurchase::GetName()  const {return itemName;}
+string ItemToPurchase::GetDescription() const {return itemDescription;}
 double ItemToPurchase::GetPrice() const {return itemPrice;}
 int ItemToPurchase::GetQuantity() const {return itemQuantity;}
+
+
+// Printers
+void ItemToPurchase::PrintCostItem(ItemToPurchase itemObject) const { //FIXME: Is const needed here? I don't think I'm modifying the object
+    double itemSubTotal = 0.0;
+
+    // Set precision to display two decimal points in standard dollar form
+
+    cout << fixed << setprecision(2);
+
+    // Output cost in format according to instructions
+    cout << itemObject.GetName() << " " << itemObject.GetQuantity() << " @ $";
+    cout << itemObject.GetPrice() << " = $";
+    itemSubTotal = itemObject.GetPrice() * itemObject.GetQuantity();
+    cout << itemSubTotal << endl;
+}
+void ItemToPurchase::PrintDescriptionItem(ItemToPurchase itemObject) const { //FIXME: Is const needed here? I don't think I'm modifying the object
+    cout << itemObject.GetName() << ": " << itemObject.GetDescription() << ".";
+}
 
 
 // Default constructor
 ItemToPurchase::ItemToPurchase() {
     itemName = "none";
+    itemDescription = "none";
     itemPrice = 0.0;
     itemQuantity = 0;
 }
--- 7-22-main-lab/ItemToPurchase.h
+++ 7-22-main-lab/ItemToPurchase.h
@@ -11,9 +11,14 @@ public:
     double GetPrice() const;
     void SetQuantity(int quantityParam);
     int GetQuantity() const;
+    void SetDescription(string descriptionParam);
+    string GetDescription() const;
+    void PrintCostItem(ItemToPurchase itemObject) const;
+    void PrintDescriptionItem(ItemToPurchase itemObject) const;
     ItemToPurchase(); // Prototype default constructor
 private:
     string itemName;
+    string itemDescription;
     double itemPrice;
     int itemQuantity;
 };
