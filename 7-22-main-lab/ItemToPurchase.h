724b55824dea35c4ab859c1fa1632b1f3b90118c
sdaffer <jacob.feltman@gmail.com>
2021-05-08 13:13:38 -0600

    Initial commit

---
+++ 7-22-main-lab/ItemToPurchase.h
@@ -0,0 +1,14 @@
+//
+// Created by jacob on 5/8/21.
+//
+
+#ifndef INC_7_22_MAIN_LAB_ITEMTOPURCHASE_H
+#define INC_7_22_MAIN_LAB_ITEMTOPURCHASE_H
+
+
+class ItemToPurchase {
+
+};
+
+
+#endif //INC_7_22_MAIN_LAB_ITEMTOPURCHASE_H
