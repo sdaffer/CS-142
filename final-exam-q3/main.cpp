45aac0b17a136ddee5e80f1104ca8d5133be3ae2
sdaffer <jacob.feltman@gmail.com>
2021-06-15 16:27:18 -0600

    Solution.

--- final-exam-q3/main.cpp
+++ final-exam-q3/main.cpp
@@ -1,6 +1,104 @@
 #include <iostream>
+using namespace std;
+
+//Define your class here
+class Door {
+    public:
+        // Default constructor
+        Door();
+
+        // Setters
+        void LockDoor();
+        void UnlockDoor();
+        void OpenDoor();
+        void CloseDoor();
+
+    private:
+        bool isOpen;
+        bool isLocked;
+};
+
+// Default constructor
+Door::Door(){
+    isOpen = false;
+    isLocked = true;
+}
+
+// Setters
+void Door::LockDoor() {
+    if (!isOpen && !isLocked) {
+        isLocked = true;
+    }
+    else {
+        if (isOpen) {
+            cout << "Sorry, the door is open." << endl;
+        }
+        if (!isOpen && isLocked) {
+            cout << "The door is already locked." << endl;
+        }
+    }
+}
+void Door::UnlockDoor() {
+    if (!isOpen && isLocked) {
+        isLocked = false;
+    }
+    else {
+        if (isOpen) {
+            cout << "Sorry, the door is open." << endl;
+        }
+        if (!isOpen && !isLocked) {
+            cout << "The door is already unlocked." << endl;
+        }
+    }
+}
+void Door::OpenDoor() {
+    if (isOpen) {
+        cout << "The door is already open." << endl;
+    }
+    else {
+        if (isLocked){
+            cout << "Sorry, the door is locked." << endl;
+        }
+        else {
+            isOpen = true;
+        }
+    }
+}
+void Door::CloseDoor() {
+    if (!isOpen) {
+        cout << "The door is already shut." << endl;
+    }
+    else {
+        isOpen = false;
+    }
+}
+
+
+//Do not edit below this line-----------------------------------------------------------------
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Door pantryDoor;
+    Door frontDoor;
+
+
+    frontDoor.LockDoor();
+    frontDoor.UnlockDoor();
+    frontDoor.UnlockDoor();
+    frontDoor.OpenDoor();
+    frontDoor.LockDoor();
+    frontDoor.UnlockDoor();
+    frontDoor.CloseDoor();
+    frontDoor.LockDoor();
+    frontDoor.LockDoor();
+    pantryDoor.OpenDoor();
+    pantryDoor.UnlockDoor();
+    pantryDoor.OpenDoor();
+    pantryDoor.OpenDoor();
+    frontDoor.CloseDoor();
+    pantryDoor.CloseDoor();
+    pantryDoor.CloseDoor();
+    pantryDoor.LockDoor();
+    pantryDoor.OpenDoor();
+
     return 0;
 }
